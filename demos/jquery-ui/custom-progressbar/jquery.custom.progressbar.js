062bf29705996030baf6bf7b5da331e16427a3c6
季维涛 <jiweitao@jiweitaodeMacBook-Air.local>
2017-04-08 15:29:57 +0800

    验证jquery widget factory功能

--- demos/jquery-ui/custom-progressbar/jquery.custom.progressbar.js
+++ demos/jquery-ui/custom-progressbar/jquery.custom.progressbar.js
@@ -3,13 +3,22 @@ $.widget('custom.progressbar', {
     options: {
         value: 0
     },
+
+    /**
+     * 插件初始化
+     * @private
+     */
     _create: function() {
         this.element.addClass('progressbar');
         this.element.html('<div class="progressbar-value"></div>');
         this._update();
     },
 
-    // Create a public method.
+    /**
+     * 设置值
+     * @param value
+     * @returns {number}
+     */
     value: function(value) {
         if ( value === undefined ) {
             return this.options.value;
@@ -20,14 +29,26 @@ $.widget('custom.progressbar', {
     },
 
     /**
-     * 设置选项值
+     * 设置一个选项值
+     * @param key 选项名
+     * @param value 选项值
+     * @private
+     */
+    _setOption: function(key, value) {
+        console.log('The ' + key + ' option will change.');
+        this._super(key, value);
+        this._update();
+    },
+
+    /**
+     * 设置多个选项值
      * @param key
      * @param value
      * @private
      */
-    _setOption: function(key, value) {
-        this.options[key] = value;
-        this._update();
+    _setOptions: function(options) {
+        console.log('The options will change.');
+        this._super(options);
     },
 
     /**
@@ -38,9 +59,11 @@ $.widget('custom.progressbar', {
      */
     _constrain: function(value) {
         if (value > 100) {
+            console.log('The value can not be greater than 100.');
             value = 100;
         }
         if (value < 0) {
+            console.log('The value can not be less than 0');
             value = 0;
         }
         return value;
@@ -53,9 +76,19 @@ $.widget('custom.progressbar', {
     _update: function() {
         console.log('update ...');
         var progress = this.options.value + "%";
-        this.element.find('.progressbar-value').css('width', this.options.value+'%');
+        this.element.find('.progressbar-value').css('width', this.options.value + '%');
+
         if (this.options.value == 100) {
             this._trigger('complete', null, {value: 100});
         }
+    },
+
+    test: function() {
+        console.log('test');
+    },
+
+    _test: function() {
+        console.log('_test');
     }
+
 });
