9536475487c5b18a9efacde4dc4d1662d2d97f52
Witt Ji(季维涛) <jiwt@inspur.com>
2017-06-09 19:06:04 +0800

    add ts demos

---
+++ demos/ts/enums/test.js
@@ -0,0 +1,6 @@
+"use strict";
+var Gender_1 = require('./Gender');
+console.log(Gender_1.Gender[Gender_1.Gender.Male]);
+console.log(Gender_1.Gender.Male);
+console.log(Gender_1.Gender[Gender_1.Gender.Female]);
+console.log(Gender_1.Gender.Female);
---
+++ demos/ts/enums/test.ts
@@ -0,0 +1,7 @@
+import {Gender} from './Gender';
+
+console.log(Gender[Gender.Male]);
+console.log(Gender.Male);
+
+console.log(Gender[Gender.Female]);
+console.log(Gender.Female);
