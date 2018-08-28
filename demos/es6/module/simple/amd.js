d5ff75c57ab1d15ab5616b4e7060a49fe7460ee1
Witt Ji(季维涛) <jiwt@inspur.com>
2018-08-28 15:18:19 +0800

    将es5、es6语法验证demo转移到单独的项目中。

--- demos/es6/module/simple/amd.js
+++
@@ -1,8 +0,0 @@
-define(function() {　　
-    var hello = function() {　　　　
-        console.log('amd say hello');　　
-    };　　
-    return {
-        hello: hello
-    };
-});
--- demos/es6/module/simple/profile.js
+++
@@ -1,6 +0,0 @@
-// profile.js
-var firstName = 'Michael';
-var lastName = 'Jackson';
-var year = 1958;
-
-export {firstName, lastName, year};
